46f92f71663d522aadaa0323440521c8d444b8b6
Weronika Garbowska (Uczeń) <wgarbowska683@technikumkreatywne.pl>
2025-10-31 13:17:52 +0100

    komentarz

---
+++ 1-pierwszy-porgram/pierwsze zadanie.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){hello world
+
+    
+	
+	
+	
+	return 0;
+
+}
---
+++ 1-pierwszy-porgram/zadanie 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	short a=10;
+	float i=3.14;
+	char s='A';
+	bool m= true;
+	cout<< "zmienna calkowita "<< a<< endl;
+	cout<< "zmienna zmniennoprzecinkowa "<< i<< endl;
+	cout<< "zmienna znakowa "<<s<< endl;
+	cout<< "zmienna logiczna "<<m<< endl;
+
+	return 0;
+	
+
+
+
+
+	
+	
+	}
