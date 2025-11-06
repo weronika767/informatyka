b9b0dae3f18e2d259a7977be54b7737c04663d20
Weronika Garbowska (Uczeń) <wgarbowska683@technikumkreatywne.pl>
2025-11-06 14:13:36 +0100

    komentarz

---
+++ 1-pierwszy-porgram/zadanie 4.cpp
@@ -0,0 +1,16 @@
+#include  <iostream>
+using namespace std;
+int main(){
+	cout<<"podaj swoje imie";
+	string imie;
+	cin>> imie;
+	cout<<"podaj swoj wiek";
+	short wiek;
+	cin>> wiek;
+	
+	
+	
+	
+	return 0;
+	}
+	
