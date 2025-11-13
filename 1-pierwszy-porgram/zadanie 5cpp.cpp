a42613f086c553c87724840b9a6c6400274e1a06
Weronika767 <wgarbowska683@technikumkreatywne.pl>
2025-11-13 14:12:45 +0100

    Komentarz

---
+++ 1-pierwszy-porgram/zadanie 5cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"podaj pierwsza liczbe";	
+int liczba1;
+cin>> liczba1;	
+
+cout<<"podaj druga liczbe";
+int liczba2;
+cin>>liczba2;
+
+int suma;
+suma=liczba1+liczba2;
+cout<<"suma: "<<suma;
+	
+	
+	
+}
