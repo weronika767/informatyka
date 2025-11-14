b5d1a47b4064c63eda348fa1e178c2e52255e1b3
Weronika767 <wgarbowska683@technikumkreatywne.pl>
2025-11-14 13:17:56 +0100

    Komentarz

--- 1-pierwszy-porgram/zadanie 5cpp.cpp
+++ 1-pierwszy-porgram/zadanie 5cpp.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-cout<<"podaj pierwsza liczbe";	
-int liczba1;
-cin>> liczba1;	
+cout<<"podaj pierwsza liczbe";	// wyswietlenie tekstu
+int liczba1;  //tworzenie zmiennei
+cin>> liczba1;	// pobrania wartosci uzytkowniaka i zapisanie w  zmiennei
 
 cout<<"podaj druga liczbe";
 int liczba2;
@@ -12,6 +12,30 @@ cin>>liczba2;
 int suma;
 suma=liczba1+liczba2;
 cout<<"suma: "<<suma;
+
+cout<<endl; //przejscie do nowej lini
+
+int roznica;
+roznica=liczba1-liczba2;
+cout<<"roznica:"<<roznica;
+cout<<endl;
+
+int iloczyn;
+iloczyn=liczba1*liczba2;
+cout<<"iloczyn:"<<iloczyn;
+cout<<endl;
+
+int iloraz;
+iloraz=liczba1/liczba2;
+cout<<"iloraz:"<<iloraz;
+cout<<endl;
+int resztazdzielenia;
+resztazdzielenia=liczba1%liczba2;
+cout<<"reszta z dzielenia:"<<resztazdzielenia;
+
+
+
+
 	
 	
 	
