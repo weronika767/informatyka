b02e772ff8e9c6a11b63749c8fb26a26e403841c
Weronika767 <wgarbowska683@technikumkreatywne.pl>
2025-11-20 11:08:55 +0100

    Komentarz

---
+++ 1-pierwszy-porgram/Untitled1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+short fuel=10;
+
+while (fuel>1)
+{
+
+//jedz dalej...
+cout<<"Masz wystarczajaco paliwa"<< endl;
+fuel -=1;// zmniejszenie paliwa o 1 litr
+}
+	
+	
+	
+	
+	
+}
+
+
