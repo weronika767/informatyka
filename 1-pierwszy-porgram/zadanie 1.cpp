8caf5f2ca11fd86bda15685cc91eec90862c14b8
Weronika767 <wgarbowska683@technikumkreatywne.pl>
2025-11-21 14:12:11 +0100

    Komentarz

---
+++ 1-pierwszy-porgram/zadanie 1.cpp
@@ -0,0 +1,33 @@
+#include  <iostream>
+using namespace std;
+int main(){
+// Znajdowanie wiekszej z dwoch liczb
+
+int liczba;
+cout<<"podaj liczbe";
+cin>>liczba;
+
+
+
+//operator and(&&): Sprawdzanie,czy liczba jest w zakresie 10-20
+if((liczba >= 10)&& (liczba <=20)) {
+cout<<"liczba znajduje sie w zaakresie w 10-20."<< endl;
+} else{
+	cout<<"liczba znajduje sie w zakresie 10-20"<< endl;
+	
+}
+
+int a,b;
+if(a>b) cout<<a;
+else cout<<b;
+cout<<endl;
+
+
+
+
+
+
+
+
+
+}
