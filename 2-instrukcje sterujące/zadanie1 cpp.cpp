a3330c46aa536c38574951e17083a96f2321514e
Weronika767 <wgarbowska683@technikumkreatywne.pl>
2025-12-12 14:12:18 +0100

    komentarz

---
+++ 2-instrukcje sterujące/zadanie1 cpp.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+		//blok instrukcji do wykonania
+	for(int i=0;i<5; i++)
+	{	//wpisz liczby
+		cout<<"i wynosi:"<< i<<endl;
+	}	
+	
+	
+	for(int i=0; i <=8; i += 2)
+	{
+		//wpisz liczby parzyste
+		cout<<"liczba parzysta:"<< i<<endl;
+	}
+	for(int i=5; i > 0; i--)
+	{ 
+	   //wpisz liczby w odwrotnej kolejnoœci
+	   cout<<"i wynosi:" << i<<endl;
+	   
+	   
+	for (int i=0; i<3; i++) {
+	   	for (int j=0; j<3; j++) {
+		   
+	   		// Cia³o wewnêtrznej pêtli
+	   		cout<<"i =" <<i << ", j="<< j <<endl;
+	
+}
+	}
+   
+	
+		   
+	   
+	
+	
+	
+	
+	
+	
